946edf3b7ff18420450391acf978912350b3af16
David Bedford <beds2004@hotmail.com>
2019-06-21 18:03:26 +0100

    Start adding styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   theme: {
-    extend: {}
+    shadows: {
+      default: '0 0 5px 0 rgba(0, 0, 0, 0.08)',
+    },
+    extend: {
+      colors: {
+        grey: 'rgba(0, 0, 0, 0.4)',
+        'light-grey': '#f5f6f9',
+        blue: '#47cdff',
+        'blue-light': '#8ae2fe'
+      }
+    }
   },
   variants: {},
   plugins: []
